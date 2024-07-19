6db64ddc598a0607f4399a79a406d477261dbc0a
steve-sharpe <150041936+steve-sharpe@users.noreply.github.com>
2024-07-19 13:58:52 -0230

    FUCK YEAH! Fixed it.

--- services/pg.bands.dal.js
+++ services/pg.bands.dal.js
@@ -1,18 +1,3 @@
-//this is the file to interact with the pgadmin database
-//it has the following functions:
-//getBands - gets all the bands
-//getBandByBandId - gets a band by its id
-//addBand - adds a band
-//updateBand - updates a band
-//deleteBand - deletes a band
-//deleteBandById - deletes a band by its id
-// band_id is the primary key
-
-//this file uses the pg module to interact with the database
-//it uses the Pool class to interact
-//the Pool class is a connection pool
-//it is used to manage multiple connections to the database
-
 const DEBUG = true;
 
 const Pool = require('pg').Pool;
@@ -25,125 +10,93 @@ const pool = new Pool({
 });
 
 
-const query = function(sql, params, callback) {
-  pool.query(sql, params, callback);
-};
+//this function gets all the bands
 
-var getBands = function() {
-  if(DEBUG) console.log("bands.pg.dal.getBands()");
-  return new Promise(function(resolve, reject) {
-    const sql = "SELECT band_id AS _id, band_name, band_singer, band_label, number_albums, favourite_album FROM bands";
-    query(sql, [], (err, result) => {
-      if (err) {
-        // logging should go here
-        if(DEBUG) console.log(err);
-        reject(err);
-      } else {
-        resolve(result.rows);
-      }
-    });
-  });
-}
-
-var getBandByBandId = function(id) {
-  if(DEBUG) console.log("bands.pg.dal.getBandByBandId()");
-  return new Promise(function(resolve, reject) {
-    const sql = "SELECT band_id AS _id, band_name FROM bands WHERE band_id = $1";
-    query(sql, [id], (err, result) => {
-      if (err) {
-        // logging should go here
-        if(DEBUG) console.log(err);
-        reject(err);
-      } else {
-        resolve(result.rows);
-      }
-    });
-  });
-}
-
-var addBand = function(band_name, band_singer, band_label, number_albums, favourite_album) {
-  if(DEBUG) console.log("bands.pg.dal.addBand()");
-  return new Promise(function(resolve, reject) {
-    const sql = "INSERT INTO public.bands (band_name, band_singer, band_label, number_albums, favourite_album) VALUES ($1, $2, $3, $4, $5);";
-    query(sql, [band_name, band_singer, band_label, number_albums, favourite_album], (err, result) => {
-      if (err) {
-        if(DEBUG) console.log(err);
-        reject(err);
-      } else {
-        resolve(result.rows);
-      }
-    }
-    );
-  });
-}
-
-var putBand = function(band_id, band_name, band_singer, band_label, number_albums, favourite_album) {
-  if(DEBUG) console.log("bands.pg.dal.putBand()");
-  return new Promise(function(resolve, reject) {
-    const sql = "UPDATE public.bands SET band_name=$2, band_singer=$3, band_label=$4, number_albums=$5, favourite_album=$6 WHERE band_id=$1;";
-    query(sql, [band_id, band_name, band_singer, band_label, number_albums, favourite_album], (err, result) => {
-      if (err) {
-        reject(err);
-      } else {
-        resolve(result.rows);
-      }
-    });
-  });
-}
-
-var patchBand = function(band_id, band_name, band_singer, band_label, number_albums, favourite_album) {
-  if(DEBUG) console.log("bands.pg.dal.patchBand()");
-  return new Promise(function(resolve, reject) {
-    const sql = "UPDATE public.bands SET band_name=$2, band_singer=$3, band_label=$4, number_albums=$5, favourite_album=$6 WHERE band_id=$1;";
-    query(sql, [band_id, band_name, band_singer, band_label, number_albums, favourite_album], (err, result) => {
-      if (err) {
-        reject(err);
-      } else {
-        resolve(result.rows);
-      }
-    });
-  });
-}
-
-var deleteBand = function(id) {
-  if(DEBUG) console.log("bands.pg.dal.deleteBand()");
-  return new Promise(function(resolve, reject) {
-    const sql = "DELETE FROM public.bands WHERE band_id = $1;";
-    query(sql, [id], (err, result) => {
-      if (err) {
-        reject(err);
-      } else {
-        resolve(result.rows);
-      }
-    });
+async function getBands() {
+  if (DEBUG) console.log("getBands called");
+  const client = await pool.connect();
+  try {
+    const res = await client.query('SELECT * FROM bands');
+    return res.rows;
   }
-  );
+  finally {
+    client.release();
+  }
 }
 
-var deleteBandById = function(id) {
-  if(DEBUG) console.log("bands.pg.dal.deleteBandById()");
-  return new Promise(function(resolve, reject) {
-    const sql = "DELETE FROM public.bands WHERE band_id = $1;";
-    query(sql, [id], (err, result) => {
-      if (err) {
-        reject(err);
-      } else {
-        resolve(result.rows);
-      }
-    });
+//this function gets a band by its id
+
+async function getBandByBandId(id) {
+  if (DEBUG) console.log("getBandByBandId called");
+  const client = await pool.connect();
+  try {
+    const res = await client.query('SELECT * FROM bands WHERE band_id = $1', [id]);
+    return res.rows;
   }
-  );
+  finally {
+    client.release();
+  }
+}
+
+//this function adds a band
+
+async function addBand(band_name, band_singer, band_label, number_albums, favourite_album) {
+  if (DEBUG) console.log("addBand called");
+  const client = await pool.connect();
+  try {
+    await client.query('INSERT INTO bands (band_name, band_singer, band_label, number_albums, favourite_album) VALUES ($1, $2, $3, $4, $5)', [band_name, band_singer, band_label, number_albums, favourite_album]);
+  }
+  finally {
+    client.release();
+  }
+}
+
+//this function updates a band
+
+async function updateBand(id, band_name, band_singer, band_label, number_albums, favourite_album) {
+  if (DEBUG) console.log("updateBand called");
+  const client = await pool.connect();
+  try {
+    await client.query('UPDATE bands SET band_name = $1, band_singer = $2, band_label = $3, number_albums = $4, favourite_album = $5 WHERE band_id = $6', [band_name, band_singer, band_label, number_albums, favourite_album, id]);
+  }
+  finally {
+    client.release();
+  }
+}
+
+//this function deletes a band
+
+async function deleteBand(band) {
+  if (DEBUG) console.log("deleteBand called");
+  const client = await pool.connect();
+  try {
+    await client.query('DELETE FROM bands WHERE band_id = $1', [band]);
+  }
+  finally {
+    client.release();
+  }
+}
+
+//this function deletes a band by its id
+
+async function deleteBandById(id) {
+  if (DEBUG) console.log("deleteBandById called");
+  const client = await pool.connect();
+  try {
+    await client.query('DELETE FROM bands WHERE band_id = $1', [id]);
+  }
+  finally {
+    client.release();
+  }
 }
 
 module.exports = {
-  getBands: getBands,
-  getBandByBandId: getBandByBandId,
-  addBand: addBand,
-  putBand: putBand,
-  patchBand: patchBand,
-  deleteBand: deleteBand,
-  deleteBandById: deleteBandById
+  getBands,
+  getBandByBandId,
+  addBand,
+  updateBand,
+  deleteBand,
+  deleteBandById,
 };
-
 
 

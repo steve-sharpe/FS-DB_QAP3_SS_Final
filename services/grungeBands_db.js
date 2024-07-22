e0689f0ecd9d70db3ed9d680c9001c6570e1a113
steve-sharpe <150041936+steve-sharpe@users.noreply.github.com>
2024-07-22 10:27:03 -0230

    Deleted unused files

--- services/grungeBands_db.js
+++
@@ -1,9 +0,0 @@
-const Pool = require('pg').Pool
-const pool = new Pool({
-  user: 'postgres',
-  host: 'localhost',
-  database: 'grungeBands',
-  password: '1234',
-  port: 5433,
-});
-module.exports = pool;
